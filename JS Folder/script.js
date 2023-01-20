7610d1e5e7a78d847db60954bad622e89ee8b428
vk-develops <123179219+vk-develops@users.noreply.github.com>
2023-01-20 20:00:26 +0530

    initial yfe site

---
+++ JS Folder/script.js
@@ -0,0 +1,16 @@
+
+
+// window.addEventListener("scroll", function(){
+//     var nav = document.querySelector("nav");
+//     nav.classList.toggle("active", window.scrollY > 0);
+// })
+
+
+// var NavLinks = document.getElementById("NavLink")
+
+// function showMenu(){
+//     NavLinks.style.right = "0";
+// }
+// function hideMenu(){
+//     NavLinks.style.right = "-250px";
+// }
